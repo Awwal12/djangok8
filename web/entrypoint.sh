14976cf8e584b2f97c6b679322ab4dff881ab200
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-10 23:41:02 +0100

    final update

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
-cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm djangok8.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+
+/opt/venv/bin/gunicorn djangok8.wsgi:application --bind "0.0.0.0:${APP_PORT}"
