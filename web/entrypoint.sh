bd932a1841a4c7939580b1b4e7d17cb018f5e173
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-11 15:15:01 +0100

    container up and running

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 
-/opt/venv/bin/gunicorn djangok8.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+# /opt/venv/bin/gunicorn djangok8.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/gunicorn wordjolt.wsgi:application --bind "0.0.0.0:${APP_PORT}"
